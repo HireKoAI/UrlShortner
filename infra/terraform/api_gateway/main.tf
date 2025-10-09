f16326a09700b9a3055e7164b5362b8dddc5699c
lnaiduay <lohith.naidu@walgreens.com>
2025-10-09 16:09:02 -0700

    save changes

--- infra/terraform/api_gateway/main.tf
+++ infra/terraform/api_gateway/main.tf
@@ -104,7 +104,11 @@ resource "aws_lambda_permission" "api_gateway_invoke" {
 # API Gateway Deployment
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  variables   = { "timestamp" = timestamp() }
+
+  triggers = {
+    # Only redeploy when the REST API body changes (i.e., when paths/config change)
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.rest_api.body))
+  }
 
   depends_on = [
     aws_api_gateway_method.methods,
