b07ca2a796b8dfa55c591b10f1df90533e37b3ef
lnaiduay <lohith.naidu@walgreens.com>
2025-10-08 20:49:53 -0700

    update terraform api gateway replacement cadance

--- infra/terraform/api_gateway/main.tf
+++ infra/terraform/api_gateway/main.tf
@@ -100,7 +100,6 @@ resource "aws_lambda_permission" "api_gateway_invoke" {
 # API Gateway Deployment
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  variables   = { "timestamp" = timestamp() }
 
   triggers = {
     redeployment = sha1(jsonencode([
@@ -130,6 +129,10 @@ resource "aws_api_gateway_stage" "stage" {
 
   tags = {
     Name = "${var.rest_api_name}-${var.stage_name}"
+  }
+  
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -164,4 +167,6 @@ resource "aws_api_gateway_usage_plan" "main" {
   tags = {
     Name = "${var.rest_api_name}-usage-plan"
   }
+  
+  depends_on = [aws_api_gateway_stage.stage]
 } 
