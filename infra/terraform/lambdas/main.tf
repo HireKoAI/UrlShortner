188230a0c8683f9d8e0de7e15feb68cace0be4f4
NaveenSaiKumarDukka <naveen.santosh333@gmail.com>
2025-10-21 23:42:58 -0500

    policy changes

--- infra/terraform/lambdas/main.tf
+++ infra/terraform/lambdas/main.tf
@@ -1,8 +1,28 @@
+locals {
+  triggers = tomap({
+    "sns"= "sns.amazonaws.com",
+    "api-gateway" = "apigateway.amazonaws.com"
+  })
+
+  admin_policies = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRDSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+    "arn:aws:iam::aws:policy/CloudFrontFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSNSFullAccess",
+    "arn:aws:iam::aws:policy/CloudWatchFullAccess",
+    "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess",
+    "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
+  ]
+}
+
 # Data source to get the IAM role for Lambda
 data "aws_iam_role" "lambda_role" {
   count = var.role_arn == "" ? 1 : 0
   name  = "${split("-", var.lambda_name)[0]}-${split("-", var.lambda_name)[1]}-lambda-dynamodb-role"
 }
+
 
 # Lambda function
 resource "aws_lambda_function" "lambda_function" {
@@ -28,6 +48,28 @@ resource "aws_lambda_function" "lambda_function" {
     aws_cloudwatch_log_group.lambda_log_group,
   ]
 }
+
+# the list of permissions that are attached to the lambda role and gives lambda full access to the services it needs
+resource "aws_iam_role_policy_attachment" "admin_policy" {
+  for_each   = toset(local.admin_policies)
+  role       = aws_iam_role.lambda_role.id
+  policy_arn = each.value
+  
+}
+
+resource "aws_iam_role_policy" "lambda_full_access" {
+  name   = "LambdaFullAccessPolicy"
+  role   = aws_iam_role.lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "*"
+      Resource = "*"
+    }]
+  })
+}
+
 
 # CloudWatch log group for Lambda function
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
